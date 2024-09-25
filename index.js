61207c24e07161a47c6672475ea39a4d3cb1587f
Asfand322 <asfandsheikh321@gmail.com>
2024-09-25 13:59:44 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let fruits = prompt("enter your fruit name");
+switch(fruits) {
+    case "apple" :
+    case "mango" :   
+        document.write("its apple field")
+        console.log("its apple field");
+        break;
+    case "peach" :
+        document.write("its pesh field")
+        break;
+    default :
+    document.write("the value does not exist")     
+}
